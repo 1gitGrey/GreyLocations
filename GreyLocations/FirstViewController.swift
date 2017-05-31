db1c7a6b23a829857f158590aefe0a4c8a1bd11e
Grey Grissom <grey.grissom@aya.yale.edu>
2017-05-30 21:32:24 -0700

    Initial Commit

---
+++ GreyLocations/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GreyLocations
+//
+//  Created by Grey Markling on 5/30/17.
+//  Copyright © 2017 Grey Markling. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
